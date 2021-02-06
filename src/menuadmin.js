f2405c73354ddd43d9793e5a75eb630cc70219d5
ArabeZAP <77598136+ArabeZAP@users.noreply.github.com>
2021-02-06 01:25:19 -0300

    Add files via upload

---
+++ src/menuadmin.js
@@ -0,0 +1,32 @@
+const menuadmin = (prefix, pushname) => {
+ return `Slv aqui e o menu dos admins, mais para usar tem q dar adm pra mim bb kk
+ 
+ ◪ *COMANDO DOS ADMINS*
+ │
+ ├─ ❏ ${prefix}opengc
+ ├─ ❏ ${prefix}closegc
+ ├─ ❏ ${prefix}promote
+ ├─ ❏ ${prefix}demote
+ ├─ ❏ ${prefix}tagall
+ ├─ ❏ ${prefix}tagall2
+ ├─ ❏ ${prefix}tagall3
+ ├─ ❏ ${prefix}tagall4
+ ├─ ❏ ${prefix}tagall5
+ ├─ ❏ ${prefix}add
+ ├─ ❏ ${prefix}remover
+ ├─ ❏ ${prefix}listadmins
+ ├─ ❏ ${prefix}linkgroup
+ ├─ ❏ ${prefix}leave
+ ├─ ❏ ${prefix}welcome
+ ├─ ❏ ${prefix}nsfw
+ ├─ ❏ ${prefix}leveling
+ ├─ ❏ ${prefix}level
+ ├─ ❏ ${prefix}delete
+ ├─ ❏ ${prefix}simih
+ └─ ❏ ${prefix}ownergroup
+ `
+
+
+}
+
+exports.menuadmin = menuadmin
---
+++ src/paulomenu.js
@@ -0,0 +1,12 @@
+const paulomenu = (prefix, pushname) => {
+    return `◪ *COMANDOS DO BOT DO ÁRABE*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.paulomenu = paulomenu
